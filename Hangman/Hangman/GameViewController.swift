a7c0aa56a1b1af36f53dbcd93735b2d4abfc76e9
Jamiegaskin <jamiegaskin@berkeley.edu>
2016-03-11 19:49:50 -0800

    finished part 1

--- Hangman/Hangman/GameViewController.swift
+++ Hangman/Hangman/GameViewController.swift
@@ -10,13 +10,34 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+
+    @IBOutlet weak var incorrects: UILabel!
+    @IBOutlet weak var textGuess: UITextField!
+    @IBOutlet weak var letters: UILabel!
+    @IBOutlet weak var picture: UIImageView!
+    var index = 0
+    var pictureIndex = 1
+    var currPhrase: String?
+    let pictures: [String] = ["hangman1.gif", "hangman2.gif", "hangman3.gif", "hangman4.gif", "hangman5.gif", "hangman6.gif", "hangman7.gif"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         let hangmanPhrases = HangmanPhrases()
-        var phrase = hangmanPhrases.getRandomPhrase()
+        let phrase = hangmanPhrases.getRandomPhrase()
         print(phrase)
+        currPhrase = phrase
+        incorrects.text = " "
+        var lettersString = ""
+        for char in phrase.characters {
+            if char == " " {
+                lettersString += "  "
+            } else {
+                lettersString += " _"
+            }
+        }
+        letters.text = lettersString
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,5 +55,22 @@ class GameViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func incorrectAction(sender: AnyObject) {
+        if textGuess.text?.characters.count == 1 && pictureIndex < 7 {
+            incorrects.text = incorrects.text! + " " + textGuess.text!
+            picture.image = UIImage(named: pictures[pictureIndex++])
+        }
+    }
 
+    @IBAction func correctAction(sender: AnyObject) {
+        if textGuess.text?.characters.count == 1 && index < currPhrase?.characters.count {
+            let replaceChar = currPhrase![currPhrase!.startIndex.advancedBy(index)]
+            let myRange = letters.text!.startIndex.advancedBy(index)..<letters.text!.startIndex.advancedBy(index + 2)
+            letters.text = letters.text!.stringByReplacingCharactersInRange(myRange, withString: String(replaceChar))
+            index++;
+            if replaceChar == " " {
+                correctAction(sender)
+            }
+        }
+    }
 }
